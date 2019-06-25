3146d175db1f702a279a6424e1c1f2e28def9e19
Kevin Krato <kratok08@gmail.com>
2019-06-25 09:13:59 -0600

    Add NavBar route path

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,13 @@ import "./App.scss";
 import { Route } from "react-router-dom";
 import LoginPage from "../LoginPage/LoginPage";
 import MainPage from "../MainPage/MainPage";
+import NavBar from "../NavBar/NavBar";
 
 function App() {
   return (
     <main className="App">
       <Route exact path="/" component={LoginPage} />
+      <Route path="/main" component={NavBar} />
       <Route exact path="/main" component={MainPage} />
     </main>
   );
