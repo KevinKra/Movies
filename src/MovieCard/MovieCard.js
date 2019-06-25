ecc9b9eea4681336e876a756dfb89cb5b5ac38e5
Kevin Krato <kratok08@gmail.com>
2019-06-25 11:47:47 -0600

    Handle hover variation

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -2,10 +2,35 @@ import React, { Component } from "react";
 import "./MovieCard.scss";
 
 export class MovieCard extends Component {
+  state = {
+    showData: false
+  };
+  handleHover = () => {
+    const toggle = this.state.showData;
+    this.setState({ showData: !toggle });
+  };
   render() {
     return (
-      <div className="MovieCard">
-        <h4>Movie Card</h4>
+      <div
+        tabIndex="0"
+        className="MovieCard"
+        onFocus={this.props.extendInfo}
+        onBlur={this.props.extendInfo}
+        onMouseEnter={this.handleHover}
+        onMouseLeave={this.handleHover}
+      >
+        <div className={`overlay ${this.state.showData ? "fadeIn" : "hidden"}`}>
+          <i>X</i>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab
+            provident commodi, cumque voluptate blanditiis similique tempora
+            debitis aperiam sunt cum iusto quos ipsum architecto vel alias
+            quaerat ut reprehenderit quod?
+          </p>
+        </div>
+        <h4 className={`overlay ${this.state.showData ? "hidden" : "fadeIn"}`}>
+          Movie Card {this.props.name}
+        </h4>
       </div>
     );
   }
