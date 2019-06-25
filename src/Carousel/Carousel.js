84396076d0f5fb4b439d0540f8fbf05065ed0286
Kevin Krato <kratok08@gmail.com>
2019-06-25 09:55:00 -0600

    Add animation

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -5,7 +5,7 @@ import MovieCard from "../MovieCard/MovieCard";
 export class Carousel extends Component {
   render() {
     return (
-      <section className="carousel-container">
+      <section className="carousel-container" data-aos="fade-left">
         <h4>Name</h4>
         <div className="carousel">
           <MovieCard />
--- src/HeroGrid/HeroGrid.js
+++ src/HeroGrid/HeroGrid.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import AOS from "aos";
 import "./HeroGrid.scss";
 
 class HeroGrid extends Component {
   render() {
+    AOS.init();
     return (
-      <div className="HeroGrid">
+      <div className="HeroGrid" data-aos="fade-right">
         <h3 className="item1">GridItem 1</h3>
         <h3 className="item2">GridItem 2</h3>
         <h3 className="item3">GridItem 3</h3>
