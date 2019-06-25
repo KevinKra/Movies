bc62afccc5614c9908eea2099c5167ae05d0c5b1
Kevin Krato <kratok08@gmail.com>
2019-06-25 16:09:48 -0600

    Handle animation conditions

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,26 +1,75 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./Carousel.scss";
 import MovieCard from "../MovieCard/MovieCard";
+import infoExtend from "../InfoExtend/InfoExtend";
+import InfoExtend from "../InfoExtend/InfoExtend";
 
 export class Carousel extends Component {
   state = {
-    extendInfo: false
+    extendInfo: false,
+    hovering: false,
+    currentSelection: 1,
+    films: [1, 2, 3]
   };
 
-  toggleExtendInfo = () => {
-    const toggle = this.state.extendInfo;
-    this.setState({ extendInfo: !toggle });
+  expandExtendInfo = () => {
+    this.setState({ extendInfo: true });
+  };
+
+  collapseExtendInfo = () => {
+    this.setState({ extendInfo: false });
+  };
+
+  handleMouseOver = () => {
+    this.setState({ hovering: true });
+  };
+
+  handleMouseOut = () => {
+    this.setState({ hovering: false });
+  };
+
+  //proof of concept, will need to dump/modify with proper data.
+  updateSelection = number => {
+    this.setState({ currentSelection: number, extendInfo: true });
+  };
+
+  //proof of concept, will need to dump/modify with proper data.
+  renderExtData = () => {
+    return this.state.films[this.state.currentSelection - 1];
   };
 
   render() {
     return (
-      <section className="carousel-container" data-aos="fade-left">
+      <section
+        onMouseLeave={this.collapseExtendInfo}
+        className={`carousel-container ${
+          this.state.extendInfo || this.state.hovering
+            ? "carousel-expanded"
+            : "carousel-collapse"
+        }`}
+      >
         <h4>Name</h4>
         <div className="carousel">
-          <MovieCard name="1" extendInfo={this.toggleExtendInfo} />
-          <MovieCard name="2" extendInfo={this.toggleExtendInfo} />
-          <MovieCard name="3" extendInfo={this.toggleExtendInfo} />
+          {this.state.films.map((film, i) => {
+            return (
+              <MovieCard
+                name={film}
+                key={i}
+                expandExtendInfo={this.expandExtendInfo}
+                // collapseExtendInfo={this.collapseExtendInfo}
+                toggle={this.toggle}
+                updateSelection={this.updateSelection}
+              />
+            );
+          })}
         </div>
+        {this.state.extendInfo || this.state.hovering ? (
+          <InfoExtend
+            handleMouseOut={this.handleMouseOut}
+            handleMouseOver={this.handleMouseOver}
+            renderExtData={this.renderExtData}
+          />
+        ) : null}
       </section>
     );
   }
