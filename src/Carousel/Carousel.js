3c1c0923431641729c23e12ce68a99dbb71cb295
Kevin Krato <kratok08@gmail.com>
2019-06-25 11:48:58 -0600

    Add toggle functionality on click event

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -3,12 +3,23 @@ import "./Carousel.scss";
 import MovieCard from "../MovieCard/MovieCard";
 
 export class Carousel extends Component {
+  state = {
+    extendInfo: false
+  };
+
+  toggleExtendInfo = () => {
+    const toggle = this.state.extendInfo;
+    this.setState({ extendInfo: !toggle });
+  };
+
   render() {
     return (
       <section className="carousel-container" data-aos="fade-left">
         <h4>Name</h4>
         <div className="carousel">
-          <MovieCard />
+          <MovieCard name="1" extendInfo={this.toggleExtendInfo} />
+          <MovieCard name="2" extendInfo={this.toggleExtendInfo} />
+          <MovieCard name="3" extendInfo={this.toggleExtendInfo} />
         </div>
       </section>
     );
