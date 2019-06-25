e034e6738dea4954492d634c31a2cba1db415b08
Kevin Krato <kratok08@gmail.com>
2019-06-25 10:30:24 -0600

    Set up NavLinks

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./NavBar.scss";
 
 export default function NavBar() {
   return (
-    <nav className="NavBar">
+    <nav className="NavBar" data-aos="fade" data-aos-anchor="#trigger-opacity">
       <div className="nav-width">
         <h3>Galleria</h3>
-        <ul>
-          <li>Home</li>
-          <li>Favorites</li>
-          <li>Login</li>
-        </ul>
+        <div className="NavLinks">
+          <NavLink to="/home" activeClassName="selected">
+            Home
+          </NavLink>
+          <NavLink to="/Favorites" activeClassName="selected">
+            Favorites
+          </NavLink>
+          <NavLink to="/Login" activeClassName="selected">
+            Login
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
