802ba5af70622eb2647fd490a1a872fda658a237
Kevin Krato <kratok08@gmail.com>
2019-06-25 08:33:51 -0600

    Create HeroGrid component and build grid

--- src/HeroGrid/HeroGrid.js
+++ src/HeroGrid/HeroGrid.js
@@ -5,7 +5,12 @@ class HeroGrid extends Component {
   render() {
     return (
       <div className="HeroGrid">
-        <h3>HeroGrid</h3>
+        <h3 className="item1">GridItem 1</h3>
+        <h3 className="item2">GridItem 2</h3>
+        <h3 className="item3">GridItem 3</h3>
+        <h3 className="item4">GridItem 4</h3>
+        <h3 className="item5">GridItem 5</h3>
+        <h3 className="item6">GridItem 6</h3>
       </div>
     );
   }
