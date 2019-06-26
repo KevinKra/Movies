5c403fd190f4994f0f4799a1d909cd907085739c
Kevin Krato <kratok08@gmail.com>
2019-06-26 11:43:08 -0600

    Build dynamic inline styles

--- src/HeroGrid/HeroGrid.js
+++ src/HeroGrid/HeroGrid.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import "./HeroGrid.scss";
+import image1 from "../images/woman-climbing.jpg";
+import image2 from "../images/man-in-forest.jpg";
+import image3 from "../images/kids-show.jpg";
+import image4 from "../images/live-game.jpg";
+import image5 from "../images/originals-kid.jpg";
+import image6 from "../images/sitting-at-car.jpg";
 
 class HeroGrid extends Component {
+  divStyle = (image, position) => {
+    return {
+      backgroundImage: `url(${image})`,
+      backgroundPosition: position,
+      backgroundSize: "cover"
+    };
+  };
   render() {
     return (
       <div className="HeroGrid" data-aos="fade-right">
-        <h3 className="item1">GridItem 1</h3>
-        <h3 className="item2">GridItem 2</h3>
-        <h3 className="item3">GridItem 3</h3>
-        <h3 className="item4">GridItem 4</h3>
-        <h3 className="item5">GridItem 5</h3>
-        <h3 className="item6">GridItem 6</h3>
+        <div className="item1" style={this.divStyle(image1, "center")}>
+          <p>Documentaries</p>
+        </div>
+        <div className="item2" style={this.divStyle(image3, "center top")}>
+          <p>For all ages</p>
+        </div>
+        <div className="item3" style={this.divStyle(image6, "center")}>
+          <p>Music Videos</p>
+        </div>
+        <div className="item4" style={this.divStyle(image4, "center")}>
+          <p>Live TV</p>
+        </div>
+        <div className="item5" style={this.divStyle(image2, "center")}>
+          <p>Watch Horror films</p>
+        </div>
+        <div className="item6" style={this.divStyle(image5, "center")}>
+          <p>Galleria Originals</p>
+        </div>
       </div>
     );
   }
