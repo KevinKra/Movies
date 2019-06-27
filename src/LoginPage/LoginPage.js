c05229c97a18e7319878f85e68172ab54e13770d
Kevin Krato <kratok08@gmail.com>
2019-06-27 16:52:56 -0600

    Import Form and convert to stateless funcitonal comp

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,31 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
+import Form from "../Form/Form";
 import "./LoginPage.scss";
 
-class LoginPage extends Component {
-  state = {
-    username: "",
-    password: "",
-    usernameActive: false,
-    passwordActive: false
-  };
-
-  toggleFocus = target => {
-    const toggle = this.state[target];
-    this.setState({ [target]: !toggle });
-  };
-
-  captureInput = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.history.push("/main");
-  };
-
+class LoginPage extends React.Component {
   render() {
-    const { usernameActive, passwordActive, username, password } = this.state;
     return (
       <div className="LoginPage">
         <aside className="login-image" />
@@ -33,54 +11,7 @@ class LoginPage extends Component {
           <header>
             <h1 className="main-title">Galleria</h1>
           </header>
-          <form onSubmit={this.handleSubmit}>
-            <h2>Sign In</h2>
-            <div className="input-wrapper">
-              <label
-                htmlFor="username"
-                className={
-                  usernameActive || username ? "text-inside" : "text-above"
-                }
-              >
-                Login
-              </label>
-              <input
-                type="text"
-                name="username"
-                required
-                autoComplete="off"
-                value={username}
-                onChange={this.captureInput}
-                onFocus={() => this.toggleFocus("usernameActive")}
-                onBlur={() => this.toggleFocus("usernameActive")}
-              />
-            </div>
-            <div className="input-wrapper">
-              <label
-                htmlFor="password"
-                className={
-                  passwordActive || password ? "text-inside" : "text-above"
-                }
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                required
-                autoComplete="off"
-                value={this.state.password}
-                onChange={this.captureInput}
-                onFocus={() => this.toggleFocus("passwordActive")}
-                onBlur={() => this.toggleFocus("passwordActive")}
-              />
-            </div>
-            <button className="sign-in">Sign in</button>
-            <div className="new-member">
-              <p>Don't have an account?</p>
-              <button className="sign-up">Sign up now.</button>
-            </div>
-          </form>
+          <Form />
         </section>
       </div>
     );
