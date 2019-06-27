1dfbf65a5ff1fdd1a6444a6e8a206e3b9270efb4
Kevin Krato <kratok08@gmail.com>
2019-06-27 09:43:49 -0600

    Add additional JSX elements

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -20,23 +20,34 @@ class LoginPage extends Component {
   render() {
     return (
       <div className="LoginPage">
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="username"> Login</label>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.captureInput}
-          />
-          <label htmlFor="password"> Password</label>
-          <input
-            type="text"
-            name="password"
-            value={this.state.password}
-            onChange={this.captureInput}
-          />
-          <button>Submit</button>
-        </form>
+        <aside className="login-image" />
+        <section className="login-section">
+          <header>
+            <h1 className="main-title">Galleria</h1>
+          </header>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="username"> Login</label>
+            <input
+              type="text"
+              name="username"
+              autoComplete="off"
+              value={this.state.username}
+              onChange={this.captureInput}
+            />
+            <label htmlFor="password"> Password</label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.captureInput}
+            />
+            <button>Sign in</button>
+            <div className="new-member">
+              <p>Don't have an account?</p>
+              <button className="sign-up">Sign up now.</button>
+            </div>
+          </form>
+        </section>
       </div>
     );
   }
