e57117665ed98961558efa18df2d5617fda571eb
Kevin Krato <kratok08@gmail.com>
2019-06-28 17:14:44 -0600

    Fetch data from api and update state

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import "./MainPage.scss";
 import HeroContent from "../HeroContent/HeroContent";
 import Carousel from "../Carousel/Carousel";
 import HeroGrid from "../HeroGrid/HeroGrid";
 import Footer from "../Footer/Footer";
+import API_KEY from "../api/";
 
-const MainPage = () => {
-  return (
-    <main className="MainPage">
-      <HeroContent />
-      <section className="main-content">
-        <Carousel />
-        <HeroGrid />
-        <Carousel />
-        <Carousel />
-      </section>
-      <Footer />
-    </main>
-  );
-};
+class MainPage extends Component {
+  state = {
+    movies: []
+  };
+  componentDidMount() {
+    this.callData();
+  }
+
+  callData = async () => {
+    let response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?certification_country=US&api_key=${API_KEY}`
+    );
+    const movies = await response.json();
+    this.setState({ movies: movies.results });
+  };
+
+  render() {
+    return (
+      <main className="MainPage">
+        <HeroContent />
+        <section className="main-content">
+          <Carousel title="Popular movies" films={this.state.movies} />
+          <HeroGrid />
+          <Carousel />
+          <Carousel />
+        </section>
+        <Footer />
+      </main>
+    );
+  }
+}
 
 export default MainPage;
