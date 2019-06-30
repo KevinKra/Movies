b1a32a64fb948d9b9484377d359726985b7fe2ba
Kevin Krato <kratok08@gmail.com>
2019-06-30 17:32:49 -0600

    Determine expansion size

--- src/InfoExtend/InfoExtend.js
+++ src/InfoExtend/InfoExtend.js
@@ -6,23 +6,27 @@ export default function InfoExtend(props) {
     props.information.backdrop_path
   }`;
   return (
-    <article className="expansion-container">
-      <section>
-        <h1>{props.information.title}</h1>
-        <p>{props.information.overview}</p>
-        <p>{props.information.vote_average}</p>
-        <p>{props.information.original_language}</p>
-      </section>
+    <article
+      className="expansion-container"
+      style={props.extendInfo ? { height: "500px" } : { height: "0px" }}
+    >
       <div className="background-image">
-        <div className="img-gradient" />
+        <div className="img-gradient">
+          <section>
+            <h1>{props.information.title}</h1>
+            <p>{props.information.overview}</p>
+            <p>{props.information.vote_average}</p>
+            <p>{props.information.original_language}</p>
+          </section>
+        </div>
         <img src={backgroundImage} alt="" />
         {/* <img
-            src={
-              "https://image.tmdb.org/t/p/original/m67smI1IIMmYzCl9axvKNULVKLr.jpg" ||
-              backgroundImage
-            }
-            alt=""
-          /> */}
+          src={
+            "https://image.tmdb.org/t/p/original/m67smI1IIMmYzCl9axvKNULVKLr.jpg" ||
+            backgroundImage
+          }
+          alt=""
+        /> */}
       </div>
     </article>
   );
