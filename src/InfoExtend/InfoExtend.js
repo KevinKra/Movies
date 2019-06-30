a3ea80f2dfdf7df22da5192cd98c5da8f1c7db88
Kevin Krato <kratok08@gmail.com>
2019-06-30 12:36:43 -0600

    Pass url into img jsx tag

--- src/InfoExtend/InfoExtend.js
+++ src/InfoExtend/InfoExtend.js
@@ -2,17 +2,35 @@ import React from "react";
 import "./InfoExtend.scss";
 
 export default function InfoExtend(props) {
+  const backgroundImage = `https://image.tmdb.org/t/p/w1280${
+    props.information.backdrop_path
+  }`;
   return (
-    <div
-      className="box"
-      onMouseOver={props.handleMouseOver}
-      onMouseLeave={props.handleMouseOut}
-    >
-      {/* <h1>content: {props.renderExtData()}</h1> */}
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-    </div>
+    <React.Fragment>
+      {console.log("props", props)}
+      <article
+        className="expansion-container"
+        // onMouseOver={props.handleMouseOver}
+        // onMouseLeave={props.handleMouseOut}
+      >
+        <section>
+          <h1>{props.information.title}</h1>
+          <p>{props.information.overview}</p>
+          <p>{props.information.vote_average}</p>
+          <p>{props.information.original_language}</p>
+        </section>
+        <div className="background-image">
+          <div className="img-gradient" />
+          <img src={backgroundImage} alt="" />
+          {/* <img
+            src={
+              "https://image.tmdb.org/t/p/original/m67smI1IIMmYzCl9axvKNULVKLr.jpg" ||
+              backgroundImage
+            }
+            alt=""
+          /> */}
+        </div>
+      </article>
+    </React.Fragment>
   );
 }
