aaeb275658da43af12dca208f3f2187be45668ee
Kevin Krato <kratok08@gmail.com>
2019-07-01 07:18:44 -0600

    Pass in curated data to avoid possibilty of low quality fetched data

--- src/HeroContent/HeroContent.js
+++ src/HeroContent/HeroContent.js
@@ -1,33 +1,32 @@
 import React from "react";
 import * as utils from "../utils/";
+import { curatedData } from "../data";
 import "./HeroContent.scss";
 
 export const HeroContent = props => {
   const selectFromPool = () => {
-    if (!props.films || props.films.length !== 0) {
-      const index = Math.floor(Math.random() * (props.films.length - 1));
-      const movie = props.films[index];
-      return (
-        <section className="HeroContent">
-          <div
-            className="hero-image"
-            style={utils.divStyle(
-              `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
-              "center top"
-            )}
-          />
-          <div className="opacity-filter" />
-          <div className="hero-movie-details">
-            <h2>{movie.original_title}</h2>
-            <p>{movie.overview}</p>
-            <button>Watch</button>
-          </div>
-          <div className="hero-movie-support">
-            <button disabled>More</button>
-          </div>
-        </section>
-      );
-    }
+    const index = Math.floor(Math.random() * (curatedData.length - 1));
+    const movie = curatedData[index];
+    return (
+      <section className="HeroContent">
+        <div
+          className="hero-image"
+          style={utils.divStyle(
+            `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
+            "center top"
+          )}
+        />
+        <div className="opacity-filter" />
+        <div className="hero-movie-details">
+          <h2>{movie.original_title}</h2>
+          <p>{movie.overview}</p>
+          <button>Watch</button>
+        </div>
+        <div className="hero-movie-support">
+          <button disabled>More</button>
+        </div>
+      </section>
+    );
   };
   return <React.Fragment>{selectFromPool()}</React.Fragment>;
 };
